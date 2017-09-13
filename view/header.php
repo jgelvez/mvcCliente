7b4627681450e25666b392aad7459b2631912f39
jgelvez <gelvez30@gmail.com>
2017-09-12 23:09:56 -0300

    cambios

--- view/header.php
+++ view/header.php
@@ -1,19 +1,76 @@
 <!DOCTYPE html>
 <html lang="es">
-	<head>
-		<title>Actividad III</title>
-        
-        <meta charset="utf-8" />
-        
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css" />
-        <link rel="stylesheet" href="assets/js/jquery-ui/jquery-ui.min.css" />
-        <link rel="stylesheet" href="assets/css/style.css" />
-        
-        <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
-	</head>
-    <body>
+<head>
 
-    <div class="container">
+<!-- page title -->
+<title>Actividad III</title>
 
-    
+<!-- theme css --> 
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+<link rel="stylesheet" href="assets/css/bootstrap-theme.min.css" />
+<link rel="stylesheet" href="assets/js/jquery-ui/jquery-ui.min.css" />
+<link rel="stylesheet" href="assets/css/style.css" />
+        
+<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+<link href="assets/css/structure.css" rel="stylesheet" type="text/css" />
+<link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+<link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- responsive meta tag -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta charset="UTF-8">
+
+<!-- google fonts used -->
+<link href='https://fonts.googleapis.com/css?family=Lato:400,300' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+<!-- header -->
+<header>
+	
+	<div class="header-main clearfix edge-padding header-narrow">
+		<div class="header-main-inside">
+			<div class="container clearfix">
+			
+				<div class="medium-header-container">
+					<!-- Site logo -->
+					<a href="index.php" id="site-logo"></a>
+					<!-- Mobile burger icon -->
+					<div id="mobile-nav-button">
+						<div id="mobile-nav-icon">
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+					</div>
+				</div>
+				
+				<!-- header nav menu -->
+				<nav id="header-nav">
+				
+					<h1 class="menu font-reg clearfix">ACTIVIDAD 3</h1>
+				
+				</nav>
+
+			</div>
+		</div>
+	</div>
+	
+</header>
+
+<div id="main-content" class="edge-padding">
+	
+	<!-- contenido de la pagina -->
+	<div class="page-section single-blog">
+		<section class="single-blog-main">
+			<div class="container">
+				
+				<div class="row font-light">
+				
+					<div class="col-xlarge-12 col-medium-12">
+					
